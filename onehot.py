452fb1cad4d9b7a48c4069913504dab2db629537
Shree Patel <shreepatel527@gmail.com>
2024-11-21 17:32:01 -0500

    Initial commit

---
+++ onehot.py
@@ -0,0 +1,16 @@
+import numpy as np 
+
+# this py file one hot encodes a string of RNA sequence and returns a (len, 4) array
+
+def onehote(sequence):
+    # map each nucelotide in the string as a number 
+    mapping = {"A": 0, "C": 1, "G": 2, "T": 3, "U": 3}
+    seq2 = [mapping[i] for i in sequence]
+    return np.eye(4)[seq2]
+
+# usage example 
+
+dna='ATTTACGGATTGCTGA'
+#calling onehote function
+oneHotEncodedDna= onehote(dna)
+print(oneHotEncodedDna)
